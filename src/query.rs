6267bf7816bb53f7e28aec18cfb3106c00dfe0d1
Nicolas Grislain <Nicolas.Grislain@gmail.com>
2023-09-25 17:47:41 +0200

    ok

--- src/query.rs
+++ src/query.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use qrlew::{self, Ready};
 
 /// Simplified DataType
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Deserialize, Serialize)]
@@ -15,6 +16,23 @@ enum DataType {
     Id,
 }
 
+impl From<DataType> for qrlew::DataType {
+    fn from(value: DataType) -> Self {
+        match value {
+            DataType::Boolean => qrlew::DataType::boolean(),
+            DataType::Integer => qrlew::DataType::integer(),
+            DataType::Float => qrlew::DataType::float(),
+            DataType::Text => qrlew::DataType::text(),
+            DataType::Bytes => qrlew::DataType::bytes(),
+            DataType::Date => qrlew::DataType::date(),
+            DataType::Time => qrlew::DataType::time(),
+            DataType::DateTime => qrlew::DataType::date_time(),
+            DataType::Duration => qrlew::DataType::duration(),
+            DataType::Id => qrlew::DataType::id(),
+        }
+    }
+}
+
 /// Field
 #[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Deserialize, Serialize)]
 struct Field {
@@ -22,10 +40,22 @@ struct Field {
     data_type: DataType,
 }
 
+impl From<Field> for qrlew::relation::Field {
+    fn from(value: Field) -> Self {
+        qrlew::relation::Field::from_name_data_type(value.name, value.data_type)
+    }
+}
+
 /// Schema
 #[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Deserialize, Serialize)]
 struct Schema {
     fields: Vec<Field>,
+}
+
+impl From<Schema> for qrlew::relation::Schema {
+    fn from(value: Schema) -> Self {
+        qrlew::relation::Schema::from_iter(value.fields.into_iter().map(|f| qrlew::relation::Field::from(f)))
+    }
 }
 
 /// Table
@@ -37,14 +67,31 @@ struct Table {
     size: i64,
 }
 
+impl From<Table> for qrlew::Relation {
+    fn from(value: Table) -> Self {
+        qrlew::Relation::table()
+            .name(value.name)
+            .path(value.path)
+            .schema(value.schema)
+            .size(value.size)
+            .build()
+    }
+}
+
 /// Dataset
 #[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Deserialize, Serialize)]
 struct Dataset {
     tables: Vec<Table>,
 }
 
+impl From<Dataset> for qrlew::hierarchy::Hierarchy<qrlew::Relation> {
+    fn from(value: Dataset) -> Self {
+        value.tables.into_iter().map(|t| (t.path.clone(), qrlew::Relation::from(t))).collect()
+    }
+}
+
 #[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Deserialize, Serialize)]
-struct Query {
+struct Dot {
     dataset: Dataset,
     query: String,
 }
@@ -56,8 +103,8 @@ mod tests {
     use serde_json;
 
     #[test]
-    fn test_query_serialize() {
-        let query = Query {
+    fn test_dot_serialize() {
+        let query = Dot {
             dataset: Dataset { tables: vec![
                 Table {
                     name: "table_1".to_string(),
@@ -75,9 +122,9 @@ mod tests {
     }
 
     #[test]
-    fn test_query_deserialize() {
+    fn test_dot_deserialize() {
         let query_str = r#"{"dataset":{"tables":[{"name":"table_1","path":["schema","table_1"],"schema":{"fields":[{"name":"a","data_type":"Float"},{"name":"b","data_type":"Integer"}]},"size":10000}]},"query":"SELECT * FROM table_1"}"#;
-        let query: Query = serde_json::from_str(&query_str).unwrap();
+        let query: Dot = serde_json::from_str(&query_str).unwrap();
         println!("{:?}", query);
     }
 }
