ce1263d5a57c34f65da482eec72ae729f46cb43a
Nicolas Grislain <Nicolas.Grislain@gmail.com>
2023-09-30 12:14:55 +0200

    ok

--- src/auth.rs
+++ src/auth.rs
@@ -1,4 +1,5 @@
 use super::{Error, Result};
+use rand;
 use rsa::{
     Pkcs1v15Encrypt, RsaPrivateKey, RsaPublicKey,
     pkcs1v15::{SigningKey, VerifyingKey},
@@ -8,18 +9,16 @@ use rsa::{
 
 pub struct Authenticator {
     private_key: RsaPrivateKey,
-    public_key: RsaPublicKey,
     signing_key: SigningKey<Sha256>,
     verifying_key: VerifyingKey<Sha256>,
 }
 
 impl Authenticator {
     pub fn new(private_key: RsaPrivateKey) -> Self {
-        let public_key = RsaPublicKey::from(&private_key);
-        let signing_key = SigningKey::<Sha256>::new(&private_key);
+        let signing_key = SigningKey::<Sha256>::new(private_key);
         let verifying_key = signing_key.verifying_key();
         Authenticator {
-            private_key, public_key, signing_key, verifying_key
+            private_key, signing_key, verifying_key
         }
     }
 
@@ -33,11 +32,11 @@ impl Authenticator {
         &self.private_key
     }
 
-    pub fn public_key(&self) -> &RsaPublicKey {
-        &self.public_key
+    pub fn signing_key(&self) -> &SigningKey<Sha256> {
+        &self.signing_key
     }
 
-    pub fn public_key(&self) -> &RsaPublicKey {
-        &self.public_key
+    pub fn verifying_key(&self) -> &VerifyingKey<Sha256> {
+        &self.verifying_key
     }
 }
