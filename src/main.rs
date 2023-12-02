639880419e98a4157b94b212457089b26793b132
Nicolas Grislain <Nicolas.Grislain@gmail.com>
2023-12-02 01:09:11 +0100

    Update server

--- src/main.rs
+++ src/main.rs
@@ -204,8 +204,6 @@ async fn main() {
 
     // run it with hyper on localhost:3000
     tracing::info!("listening on 0.0.0.0:3000");
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
